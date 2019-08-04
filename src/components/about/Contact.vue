769b2cf1f896f7185eebd88c0b368488322bf6b3
AndyTiTi <458666482@qq.com>
2019-08-04 17:26:46 +0800

    修改页面内容

--- src/components/about/Contact.vue
+++ src/components/about/Contact.vue
@@ -5,8 +5,11 @@
       <h4 class="card-title">联系我们</h4>
       <p class="card-text">1623487989@qq.com</p>
 
-      <router-link :to="{ name: 'phoneNumber' }">电话</router-link>
-      <router-link :to="{ name: 'personName' }">联系人</router-link>
+      <router-link :to="{ name: 'phoneNumber' }">电话:to</router-link>
+      |
+      <router-link to="/phone">电话-to</router-link>
+      |
+      <router-link to="/about/contact">联系人</router-link>
 
       <router-view></router-view>
     </div>
